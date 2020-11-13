278231ffa4f8627ce4f180226346b1c54ef42cce
67O7XoO4 <gazanova@free.fr>
2020-11-13 18:44:35 +0100

    added missing translation

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,21 +1,42 @@
 
 export default { 
-    title: '4 in a row !!',
-    newGame :  'New game started. {name} to play',
-    RED : 'RED',
-    YELLOW : 'YELLOW',
-    playerPaused : '{name} paused. Click to resume to let it play',
+    cancel :"Cancel",
+    cancelRestartTitle : "Cancel current game and start a new one",
+    changePlayerTypeTitle : 'Change human/computer player',
+    chooseLang :"Choose you language",
+    confirm :"Confirm",
+
+    depth :"Game level",
+
     evenGame : 'Even game :(',
+
+    level_4 :"Newbie",
+    level_5 :"Rookie",
+    level_6 :"Trainee",
+    level_7 :"Professional",
+    level_8 :"Major",
+    level_9 :"Grand master",
+
+    newGame :  'New game started. {name} to play',
+
+    playedByComputerTitle : "Played by the computer",
+    playedByHumanTitle : "Played by a human",
+    playerPaused : '{name} paused. Click to resume to let it play',
+    playerSettingsTitle :"Edit player type and level",
+    playerSuspendedTitle : 'Player is suspended. Click to make it play',
     playerWin : '{name} win !!',
 
-    playerSuspendedTitle : 'Player is suspended. Click to make it play',
-    suspendPlayerTitle : 'Click to pause the player',
-    changePlayerTypeTitle : 'Change human/computer player',
-    playedByHumanTitle : "Played by a human",
-    playedByComputerTitle : "Played by the computer",
-    cancelRestartTitle : "Cancel current game and start a new one",
+    RED : 'RED',
     restart : "Restart",
-    undoTitle : "Undo last move",
+
+    settings :"Settings",
+    suspendPlayerTitle : 'Click to pause the player',
+
+    title: '4 in a row',
+
     undo : "Undo",
-    
+    undoTitle : "Undo last move",
+
+    YELLOW : 'YELLOW',
+
 }
--- src/i18n/fr.js
+++ src/i18n/fr.js
@@ -1,21 +1,42 @@
 
 export default {
-    title: 'Puissance 4 !!',
-    newGame : 'Nouvelle partie. Au {name} de jouer.',
-    RED : 'ROUGE',
-    YELLOW : 'JAUNE',
-    playerPaused : '{name} en pause. Cliquer pour le faire jouer',
+    cancel :"Annule",
+    cancelRestartTitle : "Annule la partie et en démarre une nouvelle",
+    changePlayerTypeTitle : 'Cliquer pour changer le type de joueur (ordinateur / Humain)',
+    chooseLang :"Choisissez votre langue",
+    confirm :"Confirmer",
+
+    depth :"Niveau de jeu",
+
     evenGame : 'Match nul :(',
+
+    level_4 :"Débutant",
+    level_5 :"Novice",
+    level_6 :"Recrue",
+    level_7 :"Professionel",
+    level_8 :"Commandant",
+    level_9 :"Grand maître",
+
+    newGame : 'Nouvelle partie. Au {name} de jouer.',
+
+    playedByComputerTitle : "Joué par l'ordinateur",
+    playedByHumanTitle : "Joué par un humain",
+    playerPaused : '{name} en pause. Cliquer pour le faire jouer',
+    playerSettingsTitle :"Editer le type de joueur et son niveau",
+    playerSuspendedTitle : 'Le joueur est en pause. Cliquer pour le faire jouer.',
     playerWin : 'Le joueur {name} gagne !!',
 
-    playerSuspendedTitle : 'Le joueur est en pause. Cliquer pour le faire jouer.',
-    suspendPlayerTitle : 'Cliquer pour metre en pause le joueur',
-    changePlayerTypeTitle : 'Cliquer pour changer le type de joueur (ordinateur / Humain)',
-    playedByHumanTitle : "Joué par un humain",
-    playedByComputerTitle : "Joué par l'ordinateur",
-    cancelRestartTitle : "Annule la partie et en démarre une nouvelle",
+    RED : 'ROUGE',
     restart : "Redémarrer",
-    undoTitle : "Annule le dernier coup",
+
+    settings :"Réglages",
+    suspendPlayerTitle : 'Cliquer pour metre en pause le joueur',
+
+    title: 'Puissance 4',
+
     undo : "Annuler",
+    undoTitle : "Annule le dernier coup",
+
+    YELLOW : 'JAUNE',
 }
 
